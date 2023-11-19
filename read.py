a4a80715c994af7887d11125c2200f50ba7b27b5
sarehsoltani <sare.soltani74@gmail.com>
2023-11-19 16:03:23 -0500

    read video

--- read.py
+++ read.py
@@ -1,6 +1,17 @@
 import cv2 as cv
+#reading image
+# img = cv.imread('Resources/Photos/cat_large.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+# -215: assertion failed: cannot find and read the media we shared or it runs out of the frames
 
-img = cv.imread('Resources/Photos/cat_large.jpg')
-#handling size
-cv.imshow('Cat', img)
-cv.waitKey(0)
+#reading videos
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+# read video frame by frame
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('video', frame)
+    if cv.waitKey(20) & 0xFF ==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
