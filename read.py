06e194f3a1662eb28e0475c05699561e7d17eaeb
sarehsoltani <sare.soltani74@gmail.com>
2023-11-19 16:50:15 -0500

    resize img/video

--- read.py
+++ read.py
@@ -1,17 +1,18 @@
 import cv2 as cv
 #reading image
-# img = cv.imread('Resources/Photos/cat_large.jpg')
-# cv.imshow('Cat', img)
-# cv.waitKey(0)
+img = cv.imread('Resources/Photos/cat_large.jpg')
+cv.imshow('Cat', img)
+cv.waitKey(0)   
+# #waitkey() function of Python OpenCV allows users to display a window for given milliseconds or until any key is pressed.
 # -215: assertion failed: cannot find and read the media we shared or it runs out of the frames
 
-#reading videos
-capture = cv.VideoCapture('Resources/Videos/dog.mp4')
-# read video frame by frame
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('video', frame)
-    if cv.waitKey(20) & 0xFF ==ord('d'):
-        break
-capture.release()
-cv.destroyAllWindows()
+# #reading videos
+# capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+# # read video frame by frame
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('video', frame)
+#     if cv.waitKey(20) & 0xFF ==ord('d'):
+#         break
+# capture.release()
+# cv.destroyAllWindows()
---
+++ resize.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+
+def resizeFrame(img, scale = 0.75):
+    width = int(img.shape[1] * scale)
+    height = int(img.shape[0] * scale)
+    dimensions = (width, height)
+    resizedImg = cv.resize(img, dimensions)
+    return resizedImg
+
+#resize image
+# img = cv.imread('Resources/Photos/cat_large.jpg')
+# resizedImg = resizeFrame(img)
+# cv.imshow('Image', img)
+# cv.imshow('Image1', resizedImg)
+# # print(img.shape) #height, width, channels = img.shape 
+# cv.waitKey(0)
+
+#resize video
+vidCapture = cv.VideoCapture('Resources/Videos/dog.mp4')
+while True:
+    isTrue, frame = vidCapture.read()
+    resizedFrm = resizeFrame(frame)
+    cv.imshow('video1',frame)
+    cv.imshow('video2', resizedFrm)
+
+    if cv.waitKey(20) & 0xFF == 'q':
+        break
+vidCapture.release()
+cv.destroyAllWindows()
+
+
