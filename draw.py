2617bfe9dee8238b0f7b2f87f750c954493869fe
sarehsoltani <sare.soltani74@gmail.com>
2023-11-19 17:42:50 -0500

    put text

--- draw.py
+++ draw.py
@@ -6,10 +6,14 @@ blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('image', blank)
 # cv.waitKey(0)
 
-#draw rectangle
-cv.rectangle(blank, (0,0), (blank.shape[1]//2,250), (0.0,255), thickness=cv.FILLED)
+# #draw rectangle
+# cv.rectangle(blank, (0,0), (blank.shape[1]//2,250), (0.0,255), thickness=cv.FILLED)
 
-#draw circle
-cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 50, (0,0,255))
-cv.imshow('circle', blank)
+# #draw circle
+# cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 50, (0,0,255))
+# cv.imshow('circle', blank)
+
+# write text
+cv.putText(blank, 'Hello Sareh Jonam', (0,225), cv.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 2)
+cv.imshow('text', blank)
 cv.waitKey(0)
