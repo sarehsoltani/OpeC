b940947cd760f883377b7443d2eb6fe07b43eea8
sarehsoltani <sare.soltani74@gmail.com>
2023-11-20 16:50:35 -0500

    rotate

--- Transformation.py
+++ Transformation.py
@@ -6,10 +6,26 @@ cv.imshow('image', img)
 
 # shift image
 def translate(img, x, y):
-    TransMat = np.float32([[1,0,x],[0,1,y]])
+    transMat = np.float32([[1,0,x],[0,1,y]])
     dimensions = (img.shape[1], img.shape[0])
-    return cv.warpAffine(img, TransMat, dimensions)
+    return cv.warpAffine(img, transMat, dimensions)
 
 translated = translate(img, 100, 100)
-cv.imshow('image', translated)
+# cv.imshow('image', translated)
+
+# Rotate
+def rotate(img, angle, rotPoint = None):
+    (width, height) = img.shape[:2]
+    if rotPoint == None:
+        rotPoint = (width//2, height//2)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, scale = 1.0)
+    dimensions = (width, height)
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, 40)
+cv.imshow('Rotated Image', rotated)
+
+
+
+
 cv.waitKey(0)
