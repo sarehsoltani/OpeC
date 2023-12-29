5fe1fd92dbca401c43032d6a70e805400e85de05
sarehsoltani <sare.soltani74@gmail.com>
2023-12-28 22:31:27 -0500

    edge detection with sobel

--- Edge Detection.py
+++ Edge Detection.py
@@ -9,10 +9,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # edge detection
-canny = cv.Canny(gray,125, 175)
-cv.imshow("Canny Edges", canny)
-
-# Lablacian
+# 1. Lablacian
 """
 A Laplacian filter is an edge detector used to compute the second derivatives of an image,
 measuring the rate at which the first derivatives change. This determines 
@@ -23,5 +20,21 @@ the slope will be negative and positive ==> absolute
 lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow("Lablacian Edges", lap)
+cv.imwrite('Lablacian.jpg', lap)
+
+# 2. Sobel: it computes the gradient into two directions (x and y)
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combinedSobel = cv.bitwise_or(sobelx, sobely)
+
+cv.imshow("Sobel X", sobelx)
+cv.imshow("Sobel Y", sobely)
+cv.imshow("Combined Sobel", combinedSobel)
+cv.imwrite('CombinedSobel.jpg', combinedSobel)
+
+# 3. Canny: it is the more advanced one that uses the sobel in one of its steps
+canny = cv.Canny(gray,150, 175)
+cv.imshow("Canny Edges", canny)
+cv.imwrite('Canny.jpg', canny)
 
 cv.waitKey(0)
