6254544601a303d120cabf331a7135b394bda707
sarehsoltani <sare.soltani74@gmail.com>
2023-12-28 21:10:46 -0500

    adaptive thresholding

--- thresh.py
+++ thresh.py
@@ -15,6 +15,12 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # Simple thresholding
 threshold, thresh =  cv.threshold(gray, 140, 255, cv.THRESH_BINARY)  #threshold is 150, and thresh is the binarized image
 cv.imshow('thresh', thresh)
+cv.imwrite('simple threshold.jpg', thresh)
+
+# Adaptive thresholding: let computer find the optimal value of threshold instead of assigning it manually
+adaptiveThresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('adaptive thresh', adaptiveThresh)
+cv.imwrite('adaptivethreshold.jpg', adaptiveThresh)
 
 cv.waitKey(0)
 
