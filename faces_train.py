a9a2e732f878f6432810531678dec4f7a41f4afa
sarehsoltani <sare.soltani74@gmail.com>
2023-12-30 12:08:57 -0500

    passing path

--- faces_train.py
+++ faces_train.py
@@ -5,4 +5,9 @@ import cv2 as cv
 p = []
 for i in os.listdir('Resources/Faces/train/'):
     p.append(i)
-print(p)    
+print(p) 
+
+DIR = r'/Users/sarehsoltani/Documents/OpeCV/Resources/Faces/train/'
+
+features = []
+label = []
